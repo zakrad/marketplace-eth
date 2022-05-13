1b28a19868b5b1594941146a9f3a2bcde7c83dea
zakrad <49591476+zakrad@users.noreply.github.com>
2022-05-13 18:41:53 +0430

    more

--- components/common/index.js
+++ components/common/index.js
@@ -2,3 +2,6 @@
 export { default as Footer } from "./footer"
 export { default as Navbar } from "./navbar"
 
+export { default as Hero } from "./hero"
+export { default as Breadcrumbs } from "./breadcrumbs"
+
