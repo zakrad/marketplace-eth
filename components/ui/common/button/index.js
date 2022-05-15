5414f390e99ff9cc5df5cdc90562516d783c5e3b
zakrad <49591476+zakrad@users.noreply.github.com>
2022-05-15 22:49:38 +0430

    provider

---
+++ components/ui/common/button/index.js
@@ -0,0 +1,15 @@
+
+
+export default function Button({
+    children,
+    className = "text-white bg-indigo-600 hover:bg-indigo-700",
+    ...rest
+}) {
+    return (
+        <button
+            {...rest}
+            className={`disabled:opacity-50 disabled:cursor-not-allowed px-8 py-3 rounded-md border text-base font-medium ${className}`}>
+            {children}
+        </button>
+    )
+}
--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,14 +1,15 @@
 import { useWeb3 } from "@components/providers"
 import Link from "next/link"
+import { Button } from "@components/ui/common"
 
 
 export default function Navbar() {
-    const { connect, isWeb3Loaded } = useWeb3()
+    const { connect, isLoading, isWeb3Loaded } = useWeb3()
     return (
         <section>
             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
                 <nav className="relative" aria-label="Global">
-                    <div className="flex justify-between">
+                    <div className="flex justify-between items-center">
                         <div>
                             <Link href="/">
                                 <a
@@ -37,17 +38,21 @@ export default function Navbar() {
                                 </a>
                             </Link>
                             {
-                                isWeb3Loaded ?
-                                    <span
-                                        onClick={connect}
-                                        className="px-8 py-3 rounded-md border text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                                        Connect
-                                    </span> :
-                                    <span
-                                        onClick={connect}
-                                        className="px-8 py-3 rounded-md border text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                                        Install Metamask
-                                    </span>
+                                isLoading ?
+                                    <Button
+                                        disabled={true}
+                                        onClick={connect}>
+                                        Loading...
+                                    </Button> :
+                                    isWeb3Loaded ?
+                                        <Button
+                                            onClick={connect}>
+                                            Connect
+                                        </Button> :
+                                        <Button
+                                            onClick={() => window.open("https://metamask.io/download.html", "_blank")}>
+                                            Install Metamask
+                                        </Button>
                             }
                         </div>
                     </div>
