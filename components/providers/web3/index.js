4fd1c3bd207bf472f350ddef6e4fa1c27a7ae598
zakrad <49591476+zakrad@users.noreply.github.com>
2022-05-15 19:42:38 +0430

    web3provider

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -1,11 +1,40 @@
-const { createContext, useContext } = require("react")
+const { createContext, useContext,useEffect ,useState} = require("react")
+import detectEthereumProvider from "@metamask/detect-provider";
+import Web3 from "web3";
 
 const web3Context = createContext(null)
 
 export default function Web3Provider({ children }) {
 
+    const [web3Api, setWeb3Api] = useState({
+        provider: null,
+        web3: null,
+        contract: null,
+        isInitialized: false
+    })
+
+    useEffect(() => {
+        const loadProvider = async () => {
+            const provider = await detectEthereumProvider(web3Context)
+            if (provider) {
+                const web3 = new Web3(provider)
+                setWeb3Api({
+                    provider,
+                    web3,
+                    contract: null,
+                    isInitialized: true
+                })
+            } else {
+                setWeb3Api(api => ({ ...api, isInitialized: true }))
+                console.error("Please install Metamask")
+            }
+        }
+
+        loadProvider()
+    }, [])
+
     return (
-        <web3Context.Provider value={{ test: "Hello" }}>
+        <web3Context.Provider value={web3Api}>
             {children}
         </web3Context.Provider >
 
