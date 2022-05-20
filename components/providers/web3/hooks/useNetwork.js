c49304aa332a69e26be1ffeb8a22fc38252fe1d8
zakrad <49591476+zakrad@users.noreply.github.com>
2022-05-20 20:25:48 +0430

    CHAINid

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,19 +1,30 @@
 import useSWR from "swr"
 import { useEffect } from "react"
 
+const NETWORKS = {
+    1: "Ethereum Main Network",
+    3: "Ropsten Test Network",
+    4: "Rinkeby Test Network",
+    5: "Georli Test Network",
+    42: "Kovan Test Network",
+    56: "Binance Smart Chain",
+    1337: "Ganache",
 
+}
 export const handler = (web3, provider) => () => {
 
     const { mutate, ...rest } = useSWR(() =>
         web3 ? "web3/network" : null,
         async () => {
-            const netId = await web3.eth.net.getId()
-            return netId
+            const chainId = await web3.eth.net.getChainId()
+            return NETWORKS[chainId]
         })
 
     useEffect(() => {
         provider &&
-            provider.on("chainChanged", netId => mutate(netId))
+            provider.on("chainChanged", chainId => {
+                mutate(NETWORKS[parseInt(chainId, 16)])
+            })
     }, [web3])
 
     return {
