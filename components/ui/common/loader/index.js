3deec6fe6cdde3e03962f827839ebb4cb80e9f8d
zakrad <49591476+zakrad@users.noreply.github.com>
2022-05-27 13:43:20 +0430

    loader2

--- components/ui/common/loader/index.js
+++ components/ui/common/loader/index.js
@@ -1,12 +1,15 @@
 
+const SIZES = {
+    sm: "w-6 h-6",
+    md: "w-8 h-8",
+    lg: "w-12 h-12",
+}
 
-
-export default function Loader() {
-
+export default function Loader({ size = "md" }) {
 
 
     return (
-        <div className="spinner">
+        <div className={`spinner ${SIZES[size]}`}>
             {
                 Array.from({ length: 2 }).map((_, i) =>
                     <div
