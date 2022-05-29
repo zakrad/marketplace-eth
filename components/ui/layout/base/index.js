83afa9145d52a4f5ec42e1c67c2db88ca4c10d29
zakrad <49591476+zakrad@users.noreply.github.com>
2022-05-29 20:37:07 +0430

    secondload

--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -1,18 +1,25 @@
 import { Navbar, Footer } from "@components/ui/common";
 import { Web3Provider } from "@components/providers";
+import Script from "next/script"
 
 export default function BaseLayout({ children }) {
 
     return (
-        <Web3Provider>
-            <div className=" max-w-7xl mx-auto px-4">
-                <Navbar />
-                <div className="fit">
-                    {children}
+        <>
+            <Script
+                src="/js/truffle-contract.js"
+                strategy=""
+            />
+            <Web3Provider>
+                <div className=" max-w-7xl mx-auto px-4">
+                    <Navbar />
+                    <div className="fit">
+                        {children}
+                    </div>
                 </div>
-            </div>
-            <Footer />
-        </Web3Provider>
+                <Footer />
+            </Web3Provider>
+        </>
     )
 
 }
