3761038dc73061082d933c17f9ec29789d7b97bf
zakrad <49591476+zakrad@users.noreply.github.com>
2022-05-29 21:45:02 +0430

    purchasefinal

--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -4,7 +4,6 @@ import { Web3Provider } from "@components/providers";
 export default function BaseLayout({ children }) {
 
     return (
-
         <Web3Provider>
             <div className=" max-w-7xl mx-auto px-4">
                 <Navbar />
