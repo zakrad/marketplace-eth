f6d71fc971b0ebe11491e7b1d60c28f4ea23da13
zakrad <49591476+zakrad@users.noreply.github.com>
2022-05-30 21:23:13 +0430

    displayownedcours

--- components/providers/web3/hooks/useOwnedCourses.js
+++ components/providers/web3/hooks/useOwnedCourses.js
@@ -6,7 +6,7 @@ import useSWR from "swr";
 
 export const handler = (web3, contract) => (courses, account) => {
     const swrRes = useSWR(() =>
-        (web3 && contract && account) ? "web3/ownedCourses" : null,
+        (web3 && contract && account) ? `web3/ownedCourses/${account}` : null,
         async () => {
             const ownedCourses = []
             for (let i = 0; i < courses.length; i++) {
@@ -23,7 +23,7 @@ export const handler = (web3, contract) => (courses, account) => {
                 const ownedCourse = await contract.methods.getCourseByHash(courseHash).call()
                 if (ownedCourse.owner !== "0x0000000000000000000000000000000000000000") {
                     const normalized = normalizeOwnedCourse(web3)(course, ownedCourse)
-                    ownedCourses.push(ownedCourse)
+                    ownedCourses.push(normalized)
                 }
             }
             return ownedCourses
--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -5,28 +5,30 @@ import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 import { getAllCourses } from "@content/courses/fetcher";
 
-
-
 export default function OwnedCourses({ courses }) {
     const { account } = useAccount()
     const { ownedCourses } = useOwnedCourses(courses, account.data)
 
     return (
         <>
-            <div className="py-4 ">
+            <div className="py-4">
                 <MarketHeader />
             </div>
             <section className="grid grid-cols-1">
-                <OwnedCourseCard>
-                    <Message>
-                        My custom message!
-                    </Message>
-                    <Button>
-                        Watch the course
-                    </Button>
-                </OwnedCourseCard>
+                {ownedCourses.data?.map(course =>
+                    <OwnedCourseCard
+                        key={course.id}
+                        course={course}
+                    >
+                        {/* <Message>
+              My custom message!
+            </Message> */}
+                        <Button>
+                            Watch the course
+                        </Button>
+                    </OwnedCourseCard>
+                )}
             </section>
-
         </>
     )
 }
@@ -40,4 +42,4 @@ export function getStaticProps() {
     }
 }
 
-OwnedCourses.Layout = BaseLayout;
+OwnedCourses.Layout = BaseLayout
