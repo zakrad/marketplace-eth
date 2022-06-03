6629742cc614b359c6e74a915c839ac28be37e90
saxon <1@1.com>
2022-06-03 21:46:41 +0430

    handle locked course

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,4 +1,5 @@
 import { useAccount, useOwnedCourse } from "@components/hooks/web3";
+import { useWeb3 } from "@components/providers";
 import { Message, Modal } from "@components/ui/common";
 import { CourseHero, Curriculum, Keypoints } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
@@ -8,12 +9,15 @@ import { getAllCourses } from "@content/courses/fetcher";
 
 
 export default function Course({ course }) {
+    const { isLoading } = useWeb3()
     const { account } = useAccount()
     const { ownedCourse } = useOwnedCourse(course, account.data)
     const courseState = ownedCourse.data?.state
     // const courseState = "deactivated"
 
-    const isLoacked = courseState === "purchased" ||
+    const isLoacked =
+        !courseState ||
+        courseState === "purchased" ||
         courseState === "deactivated"
     return (
         <>
@@ -52,6 +56,7 @@ export default function Course({ course }) {
                 </Message>}
 
             <Curriculum
+                isLoading={isLoading}
                 locked={isLoacked}
                 courseState={courseState}
             />
