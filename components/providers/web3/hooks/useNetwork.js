609a3911efdcda111734f77c5ecc7631b0c532fe
saxon <1@1.com>
2022-06-03 21:18:05 +0430

    HOOK IMPROVMENT

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -20,6 +20,10 @@ export const handler = (web3, provider) => () => {
         web3 ? "web3/network" : null,
         async () => {
             const chainId = await web3.eth.net.getChainId()
+            if (!chainId) {
+                throw new Error("Cannot retreive network, Please refresh the browser.")
+            }
+
             return NETWORKS[chainId]
         })
 
