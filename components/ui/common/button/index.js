54a978328b705155666fb04351b4c7ee8feccbf2
saxon <1@1.com>
2022-06-03 14:40:07 +0430

    navbar and filter improvment

--- components/ui/common/button/index.js
+++ components/ui/common/button/index.js
@@ -9,15 +9,16 @@ export default function Button({
 }) {
 
     const variants = {
-        purple: `text-white bg-indigo-600 ${hoverable && "hover:bg-indigo-700"}`,
+        white: `text-black bg-white`,
+        purple: `text-white bg-indigo-600 ${hoverable && "hover:bg-indigo-700"} `,
         red: `text-white bg-red-600 ${hoverable && "hover:bg-red-700"} `,
-        lightPurple: `text-indigo-700 bg-indigo-100 ${hoverable && "hover:bg-indigo-200"}`,
+        lightPurple: `text-indigo-700 bg-indigo-100 ${hoverable && "hover:bg-indigo-200"} `,
 
     }
     return (
         <button
             {...rest}
-            className={`disabled:opacity-50 disabled:cursor-not-allowed px-8 py-3 rounded-md border text-base font-medium ${className} ${variants[variant]} `}>
+            className={`disabled:opacity-50 disabled:cursor-not-allowed p-2 xs:px-8 xs:py-3 rounded-md border text-base font-medium ${className} ${variants[variant]} `}>
             {children}
         </button>
     )
--- pages/marketplace/courses/manage.js
+++ pages/marketplace/courses/manage.js
@@ -10,10 +10,10 @@ export default function ManageCourses() {
 
     return (
         <>
-                <MarketHeader />
-                <CourseFilter />
+            <MarketHeader />
+            <CourseFilter />
             <section className="grid grid-cols-1">
-                <OwnedCourseCard>
+                {/* <OwnedCourseCard>
                     <div className="flex mr-2 relative rounded-md">
                         <input
                             type="text"
@@ -25,7 +25,7 @@ export default function ManageCourses() {
                             Verify
                         </Button>
                     </div>
-                </OwnedCourseCard>
+                </OwnedCourseCard> */}
             </section>
 
         </>
