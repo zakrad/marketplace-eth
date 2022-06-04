95e1e7f8a62e875f32d761de5e29a5eca27f8373
saxon <1@1.com>
2022-06-04 18:59:36 +0430

    preparing test

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -1,151 +1,154 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract CourseMarketplace{
-  
+contract CourseMarketplace {
+
   enum State {
-      Purchased,
-      Activated,
-      Deactivated
+    Purchased,
+    Activated,
+    Deactivated
   }
 
-struct Course {
-    uint id; //32
-    uint price; //32
-    bytes32 proof; //32
-    address owner; //20
-    State state; //1
-} 
+  struct Course {
+    uint id; // 32
+    uint price; // 32
+    bytes32 proof; // 32
+    address owner; // 20
+    State state; // 1
+  }
 
-//mapping courseHash to course Data
-mapping(bytes32 => Course) private ownedCourses;
-// mapping of course Id to course Hash
-mapping(uint => bytes32) private ownedCourseHash;
+  // mapping of courseHash to Course data
+  mapping(bytes32 => Course) private ownedCourses;
 
-//number of all courses + id of course
-uint private totalOwnedCourses;
-address payable private owner;
+  // mapping of courseID to courseHash
+  mapping(uint => bytes32) private ownedCourseHash;
 
-constructor(){
+  // number of all courses + id of the course
+  uint private totalOwnedCourses;
+
+  address payable private owner;
+
+  constructor() {
     setContractOwner(msg.sender);
-}
+  }
 
-/// Course is not created!
-error CourseIsNotCreated();
-/// Course has invalid state!
-error InvalidState();
-/// Course has already a owner! 
-error CourseHasOwner();
-/// only owner has an access!
-error OnlyOwner();
+  /// Course has invalid state!
+  error InvalidState();
 
-modifier onlyOwner(){
-    if(msg.sender != getContractOwner()){
-        revert OnlyOwner();
+  /// Course is not created!
+  error CourseIsNotCreated();
+
+  /// Course has already a Owner!
+  error CourseHasOwner();
+
+  /// Only owner has an access!
+  error OnlyOwner();
+
+  modifier onlyOwner() {
+    if (msg.sender != getContractOwner()) {
+      revert OnlyOwner();
     }
     _;
-}
+  }
 
-function purchaseCourse (
-    bytes16 courseId,  // 0x00000000000000000000000000003130
-    bytes32 proof  // 0x0000000000000000000000000000313000000000000000000000000000003130
-)
-external
-payable
-{ 
+  function purchaseCourse(
+    bytes16 courseId, // 0x00000000000000000000000000003130
+    bytes32 proof // 0x0000000000000000000000000000313000000000000000000000000000003130
+  )
+    external
+    payable
+  {
     bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
 
-    if(hasCourseOwnership(courseHash)){
-        revert CourseHasOwner();
+    if (hasCourseOwnership(courseHash)) {
+      revert CourseHasOwner();
     }
 
     uint id = totalOwnedCourses++;
 
     ownedCourseHash[id] = courseHash;
     ownedCourses[courseHash] = Course({
-         id: id,
-         price: msg.value,         
-         proof: proof,
-         owner: msg.sender,
-         state: State.Purchased
+      id: id,
+      price: msg.value,
+      proof: proof,
+      owner: msg.sender,
+      state: State.Purchased
     });
-}
+  }
 
-function activateCourse(bytes32 courseHash)
-external
-onlyOwner
-{
-if(!isCourseCreated(courseHash)){
-    revert CourseIsNotCreated();
-}
+  function activateCourse(bytes32 courseHash)
+    external
+    onlyOwner
+  {
+    if (!isCourseCreated(courseHash)) {
+      revert CourseIsNotCreated();
+    }
 
-    Course storage course = ownedCourses[course];
-if(course.state != State.Purchased){
-    revert InvalidState();
-}
+    Course storage course = ownedCourses[courseHash];
+
+    if (course.state != State.Purchased) {
+      revert InvalidState();
+    }
 
     course.state = State.Activated;
-}
+  }
 
-function transferOwnership(address newOwner)
-external 
-onlyOwner
-{
-setContractOwner(newOwner);
-}
+  function transferOwnership(address newOwner)
+    external
+    onlyOwner
+  {
+    setContractOwner(newOwner);
+  }
 
-function getCourseCount()
-external
-view
-returns(uint)
-{
-return totalOwnedCourses;
-}
+  function getCourseCount()
+    external
+    view
+    returns (uint)
+  {
+    return totalOwnedCourses;
+  }
 
-function getCourseHashAtIndex(uint index)
-external 
-view
-returns(bytes32)
-{
+  function getCourseHashAtIndex(uint index)
+    external
+    view
+    returns (bytes32)
+  {
     return ownedCourseHash[index];
-}
+  }
 
-function getCourseByHash(bytes32 courseHash)
-external 
-view
-returns(Course memory)
-{
+  function getCourseByHash(bytes32 courseHash)
+    external
+    view
+    returns (Course memory)
+  {
     return ownedCourses[courseHash];
-}
+  }
 
-function getContractOwner()
-public
-view
-returns(address)
-{
-return owner;
-}
+  function getContractOwner()
+    public
+    view
+    returns (address)
+  {
+    return owner;
+  }
 
-
-function setContractOwner(address newOwner) private {
+  function setContractOwner(address newOwner) private {
     owner = payable(newOwner);
-}
+  }
 
-function isCourseCreated(bytes32 courseHash)
+  function isCourseCreated(bytes32 courseHash)
     private
     view
     returns (bool)
-    {
- return ownedCourses[courseHash].owner !== 0x0000000000000000000000000000000000000000;
-}
+  {
+    return ownedCourses[courseHash].owner != 0x0000000000000000000000000000000000000000;
+  }
 
-function hasCourseOwnership(bytes32 courseHash)
-private
-view
-returns(bool)
-{
+  function hasCourseOwnership(bytes32 courseHash)
+    private
+    view
+    returns (bool)
+  {
     return ownedCourses[courseHash].owner == msg.sender;
-}
-
-
+  }
 }
