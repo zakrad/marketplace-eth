8c1444c45942219f1c0d281bb209c0bca163c6fa
saxon <1@1.com>
2022-06-04 19:27:33 +0430

    test course data

--- test/CourseMarketplace.test.js
+++ test/CourseMarketplace.test.js
@@ -7,7 +7,7 @@ const CourseMarketplace = artifacts.require("CourseMarketplace")
 contract("CourseMarketplace", accounts => {
 
     const courseId = "0x00000000000000000000000000003130";
-    const proof = "0x0000000000000000000000000000313000000000000000000000000000003130";
+    const proof = "0x0000000000000000000000000000313000000000000000000000000000003130"
     const value = "900000000";
 
     let _contract = null
@@ -21,6 +21,7 @@ contract("CourseMarketplace", accounts => {
     })
 
     describe("Purchase the new course", () => {
+        let courseHash;
 
         before(async () => {
             await _contract.purchaseCourse(courseId, proof, {
@@ -28,16 +29,28 @@ contract("CourseMarketplace", accounts => {
                 value
             })
         })
+
         it("can get the purchased course hash by index", async () => {
             const index = 0
-            const courseHash = await _contract.getCourseHashAtIndex(index)
-
+            courseHash = await _contract.getCourseHashAtIndex(index)
             const expectedHash = web3.utils.soliditySha3(
                 { type: "bytes16", value: courseId },
                 { type: "address", value: buyer },
             )
 
-            assert.equal(courseHash, expectedHash, "Course hash is not matching the purchased course")
+            assert.equal(courseHash, expectedHash, "Course hash is not maching the hash of purchased course!")
+        })
+
+        it("should match the data of the course purchased by buyer", async () => {
+            const exptectedIndex = 0
+            const exptectedState = 0
+            const course = await _contract.getCourseByHash(courseHash)
+
+            assert.equal(course.id, exptectedIndex, "Course index should be 0!")
+            assert.equal(course.price, value, `Course price should be ${value}!`)
+            assert.equal(course.proof, proof, `Course proof should be ${proof}!`)
+            assert.equal(course.owner, buyer, `Course buyer should be ${buyer}!`)
+            assert.equal(course.state, exptectedState, `Course state should be ${exptectedState}!`)
         })
     })
 })
