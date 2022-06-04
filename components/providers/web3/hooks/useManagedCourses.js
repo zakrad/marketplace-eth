d390c33dc6d9a92be2df459e39f19c77d1a90542
saxon <1@1.com>
2022-06-04 13:26:00 +0430

    fetch managed course

--- components/providers/web3/hooks/useManagedCourses.js
+++ components/providers/web3/hooks/useManagedCourses.js
@@ -1,4 +1,5 @@
 
+import { normalizeOwnedCourse } from "@utils/normalize";
 import useSWR from "swr";
 
 
@@ -8,7 +9,17 @@ export const handler = (web3, contract) => account => {
     const swrRes = useSWR(() =>
         (web3 && contract && account) ? `web3/managedCourses/${account}` : null,
         async () => {
-            const courses = [1, 2, 3, 4]
+            const courses = []
+            const courseCount = await contract.methods.getCourseCount().call()
+            for (let i = Number(courseCount) - 1; i >= 0; i--) {
+                const courseHash = await contract.methods.getCourseHashAtIndex(i).call()
+                const course = await contract.methods.getCourseByHash(courseHash).call()
+
+                if (course) {
+                    const normalized = normalizeOwnedCourse(web3)({ hash: courseHash }, course)
+                    courses.push(normalized)
+                }
+            }
 
             return courses
         }
