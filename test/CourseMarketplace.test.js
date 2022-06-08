5abe743a50b7f19bdeec59e3847354c7299b03c2
saxon <1@1.com>
2022-06-09 01:07:14 +0430

    test withdraw

--- test/CourseMarketplace.test.js
+++ test/CourseMarketplace.test.js
@@ -254,4 +254,40 @@ contract("CourseMarketplace", accounts => {
             )
         })
     })
+
+    describe("Normal Withdraw", () => {
+        const fundsToDeposit = "100000000000000000"
+        const overLimitFunds = "999999000000000000000"
+        let currentOwner = null
+        before(async () => {
+            currentOwner = await _contract.getContractOwner()
+            await web3.eth.sendTransaction({
+                from: buyer,
+                to: _contract.address,
+                fundsToDeposit
+            })
+        })
+
+        it("should fail when withdrawing with NOT owner address", async () => {
+            const value = "10000000000000000"
+            await catchRevert(_contract.withdraw(value, { from: buyer }))
+        })
+
+        it("should fail when withdrawing over limit balance", async () => {
+            await catchRevert(_contract.withdraw(overLimitFunds, { from: currentOwner }))
+        })
+
+        it("should have +0.1ETH after withdraw", async () => {
+            const ownerBalance = await getBalance(currentOwner)
+            const result = await _contract.withdraw(fundsToDeposit, { from: currentOwner })
+            const newOwnerBalance = await getBalance(currentOwner)
+            const gas = await getGas(result)
+
+            assert.equal(
+                toBN(ownerBalance).add(toBN(fundsToDeposit)).sub(toBN(gas)).toString(),
+                newOwnerBalance,
+                "The new owner balance is not correct!!"
+            )
+        })
+    })
 })
